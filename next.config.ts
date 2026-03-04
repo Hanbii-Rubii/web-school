ae893cd3903c5f8f2ae0f0bb4896e4f05149fc26
Hanbii-Rubii <168339514+Hanbii-Rubii@users.noreply.github.com>
2026-03-04 18:47:56 +0700

    fix turbopack config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {},
+
   webpack(config) {
     config.resolve.alias["@"] = path.join(__dirname, "src");
     return config;
